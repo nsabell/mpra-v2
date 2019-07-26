33419c5a50e5c4156b5389d20ab1e02340bdb5ce
Nathan Abell <nsabell@stanford.edu>
2019-07-25 20:21:40 -0700

    flash2 change

--- code/mergePairs.sh
+++ code/mergePairs.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-# Nathan Abell
-# Montgomery Lab
-# Stanford University
-
-# Usage: 01_readPairProcessing.sh <R1.fastq.gz> <R2.fastq.gz> <out_pfx>
-
 R1=$1
 R2=$2
 OUTPFX=$3
 
-export FLASH=./bin/FLASH-1.2.11/flash
-$FLASH -t 10 -o $OUTPFX -z -r 150 -f 223 -s 10 $R1 $R2
+export FLASH=./bin/flash2
+$FLASH -t 20 -o $OUTPFX -z -r 150 -f 223 -s 10 $R1 $R2
