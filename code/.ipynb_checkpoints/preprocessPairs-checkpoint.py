41e8d5375ef9428acba40450dc65b6b837fed627
nsabell <abell.nathan@gmail.com>
2021-05-12 13:17:27 -0700

    section four

--- code/.ipynb_checkpoints/preprocessPairs-checkpoint.py
+++ code/.ipynb_checkpoints/preprocessPairs-checkpoint.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
-# preprocess_merged_pairs.py <input_fastq_from_flash>
+# preprocess_merged_pairs.py <input_fastq_from_flash> <out_name>
 
 import sys
 import gzip
-import io
 
 outname = sys.argv[2]
 
-with io.BufferedWriter(gzip.open(outname, 'wb'), buffer_size=2000000000) as outfile, io.BufferedReader(gzip.open(sys.argv[1], 'rb'), buffer_size=8000000000) as file:
+with open(outname, 'w') as outfile, open(sys.argv[1], 'r')as file:
 
 	while True:
 
-		header = file.readline().decode('UTF-8')
+		header = file.readline()
 		if not header:
 			break
 		header = header.strip().split(' ')[0]
 		
-		sequence = file.readline().decode('UTF-8').strip()
-		filter = file.readline().decode('UTF-8')
-		phred = file.readline().decode('UTF-8').strip()
+		sequence = file.readline().strip()
+		filter = file.readline()
+		phred = file.readline().strip()
         
 		if len(sequence[58:-16]) >= 25: 
+			
 			toWrite = header + ":" + sequence[0:20] + '\n' + sequence[58:-16] + '\n' + filter + phred[58:-16] + '\n'
-			outfile.write(str.encode(toWrite))
+			outfile.write(toWrite)
